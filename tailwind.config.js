012c279dbf17c8553d26c7112e7c58e3d4f9f1af
Glenn Raya <rayaglenn@gmail.com>
2024-05-21 14:37:46 +0800

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from 'tailwindcss/defaultTheme'
+import forms from '@tailwindcss/forms'
+const { nextui } = require('@nextui-org/react')
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -8,15 +9,16 @@ export default {
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
         './resources/js/**/*.jsx',
+        './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans]
+            }
+        }
     },
 
-    plugins: [forms],
-};
+    plugins: [nextui()]
+}
